9f6145897146ff955fb91296c82ffefc25e5a65d
IvanHnamzck <24521101@gm.uit.edu.vn>
2026-05-31 05:56:59 +0700

    Update MainWindow.xaml.cs

--- Sprint2/MainWindow.xaml.cs
+++ Sprint2/MainWindow.xaml.cs
@@ -20,3 +20,122 @@ namespace Sprint2
             dpNgayNhap.SelectedDate =
                 DateTime.Now;
         }
+
+private void BtnThemGame_Click(
+    object sender,
+    RoutedEventArgs e)
+{
+    try
+    {
+        string tenGame =
+            txtTenGame.Text;
+
+        string loaiGame =
+            cbLoaiGame.SelectedItem.ToString();
+
+        string nhaPhatHanh =
+            txtNhaPhatHanh.Text;
+
+        int namPhatHanh =
+            int.Parse(txtNamPhatHanh.Text);
+
+        decimal triGia =
+            decimal.Parse(txtTriGia.Text);
+
+        if (!DataManager.LoaiGames.Contains(loaiGame))
+        {
+            MessageBox.Show(
+                "Loại game không tồn tại");
+
+            return;
+        }
+
+        int tuoiGame =
+            DateTime.Now.Year -
+            namPhatHanh;
+
+        if (tuoiGame >
+            DataManager.TuoiGameToiDa)
+        {
+            MessageBox.Show(
+                "Game quá tuổi cho phép");
+
+            return;
+        }
+
+        if (triGia <= 0)
+        {
+            MessageBox.Show(
+                "Trị giá phải lớn hơn 0");
+
+            return;
+        }
+
+        Game game = new Game();
+
+        game.TenGame = tenGame;
+        game.LoaiGame = loaiGame;
+        game.NhaPhatHanh = nhaPhatHanh;
+        game.NamPhatHanh = namPhatHanh;
+        game.TriGia = triGia;
+
+        dsNhap.Add(game);
+
+        if (!DataManager.DanhSachGame
+            .Any(x => x.TenGame == tenGame))
+        {
+            DataManager.DanhSachGame
+                .Add(game);
+        }
+
+        dgGames.ItemsSource = null;
+        dgGames.ItemsSource = dsNhap;
+
+        txtTongGiaTri.Text =
+            "Tổng giá trị: "
+            + dsNhap.Sum(x => x.TriGia);
+
+        ClearInput();
+    }
+    catch
+    {
+        MessageBox.Show(
+            "Dữ liệu không hợp lệ");
+    }
+}
+
+
+private void BtnLapPhieu_Click(
+    object sender,
+    RoutedEventArgs e)
+{
+    if (dsNhap.Count == 0)
+    {
+        MessageBox.Show(
+            "Chưa có game nào");
+
+        return;
+    }
+
+    decimal tongTien =
+        dsNhap.Sum(x => x.TriGia);
+
+    MessageBox.Show(
+        $"Lập phiếu thành công\n" +
+        $"Tổng giá trị: {tongTien}");
+}
+
+private void ClearInput()
+{
+    txtTenGame.Clear();
+
+    txtNhaPhatHanh.Clear();
+
+    txtNamPhatHanh.Clear();
+
+    txtTriGia.Clear();
+
+    cbLoaiGame.SelectedIndex = -1;
+}
+    }
+}
